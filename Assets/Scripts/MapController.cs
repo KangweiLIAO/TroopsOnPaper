e73309f0e9cf4fc99b5b3697662b36579fff22df
MiaoChips <Clanceyliao@gmail.com>
2020-12-25 16:52:57 -0500

    Controller script created.

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -1,9 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MapController : MonoBehaviour
 {
+    [SerializeField] private int mapWidth = 10;
+    [SerializeField] private int mapHeight = 10;
+    private Grid grid;
+
+    public GameObject MapGrid;
+
+    void Awake()
+    {
+        grid = (Grid)MapGrid.GetComponent("Grid");
+        // Debug.Log();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,6 @@ public class MapController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
